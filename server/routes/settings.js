cab5be84324fc7ed8524b148563967429e1ab22c
Ivan Franco <if@Ivans-MacBook-Pro-2.local>
2017-08-07 13:07:41 -0400

    changes in settings. Config files edited via sed

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -1,11 +1,9 @@
 let express = require('express');
 let router = express.Router();
-// let exec = require('child_process').exec;
+let exec = require('child_process').exec;
 let fs = require('fs');
 let path = require('path');
 let private_path = path.join(__dirname, '../private/');
-
-// let jsonfile = require('jsonfile');
 
 // let configfile = '/Users/if/Desktop/prynthtemp/server/private/config.json';
 // let configbuffer = require(configfile);
@@ -30,33 +28,21 @@ router.post('/setwifi', function (req, res) {
 	var configData = JSON.parse(configFile);
 
 	// console.log(configData);
-	configData.wifinetworks.push([networkname, networkpass]);
+	// configData.wifinetworks.push([networkname, networkpass]);
+	configData.wifinetworks = ([networkname, networkpass]);
 	var configJSON = JSON.stringify(configData, null, 2);
 
 	// console.log(configJSON);
 	fs.writeFileSync(private_path +'config.json', configJSON);
 
-	buffer += "country=GB\n";
-	buffer += "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n";
-	buffer += "update_config=1\n\n";
-	buffer += "network={\n";
-	buffer += '\tssid="' + networkname +'"\n';
-	buffer += '\tpassword="' + networkpass+'"\n';
-	buffer += "}";
+	var command = 'gsed -i "/#prynth start/,/#prynth end/ s/\\(ssid *= *\\).*/\\1\\"'+networkname+'\\"/ ;/#prynth start/,/#prynth end/ s/\\(password *= *\\).*/\\1\\"'+networkpass+'\\"/" /Users/if/Desktop/myconf.conf';
+	console.log(command);
 
-	// fs.writeFile("/Users/if/Desktop/myconf.conf", buffer, function (err) {
-	// 	if(err) {
-	// 		res.send('error saving file');
-	// 		return console.log(err);
-	// 	}
-	// 	else {
-	// 		// var child = exec('atom /Users/if/Desktop/myconf.conf', function (error, stdout, stderr) {
-	// 		// 	console.log(stdout);
-	// 		// });
-	//
-	// 		res.redirect('/settings');
-	// 	}
-	// });
+	var child = exec(command, function (error, stdout, stderr) {
+		console.log(stdout);
+		res.redirect('/settings');
+	});
+
 })
 
 router.post('/setdefaultscfile', function (req, res) {
@@ -71,28 +57,7 @@ router.post('/setdefaultscfile', function (req, res) {
 	var configJSON = JSON.stringify(configData, null, 2);
 
 	fs.writeFileSync(private_path +'config.json', configJSON);
-	//
-	// buffer += "country=GB\n";
-	// buffer += "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n";
-	// buffer += "update_config=1\n\n";
-	// buffer += "network={\n";
-	// buffer += '\tssid="' + networkname +'"\n';
-	// buffer += '\tpassword="' + networkpass+'"\n';
-	// buffer += "}";
-	//
-	// fs.writeFile("/Users/if/Desktop/myconf.conf", buffer, function (err) {
-	// 	if(err) {
-	// 		res.send('error saving file');
-	// 		return console.log(err);
-	// 	}
-	// 	else {
-	// 		// var child = exec('atom /Users/if/Desktop/myconf.conf', function (error, stdout, stderr) {
-	// 		// 	console.log(stdout);
-	// 		// });
-	//
-	// 		res.redirect('/settings');
-	// 	}
-	// });
+
 })
 
 
