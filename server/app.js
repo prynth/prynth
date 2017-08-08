134274ddad5e1344a29f526a87ab8c6d15deb4dd
Ivan Franco <if@Ivans-MacBook-Pro-2.local>
2017-08-08 12:24:23 -0400

    start jack from node app

--- server/app.js
+++ server/app.js
@@ -87,7 +87,7 @@ app.use(function(err, req, res, next) {
 
 ////////////
 // start jackd
-if(jackd = null){
+if(jackd == null){
 	exec('sudo jackd -P75 -dalsa -dhw:1 -p1024 -n3 -s -r44100', function (error, stdout, stderr) {
 		console.log(stdout);
 		// res.redirect('/settings');
