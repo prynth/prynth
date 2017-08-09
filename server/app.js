559db1b1c65871820c2b9bcd4361d1b8e8990239
Ivan Franco <if@Ivans-MacBook-Pro-2.local>
2017-08-09 12:38:48 -0400

    experimenting with child processes for launching side apps

--- server/app.js
+++ server/app.js
@@ -91,19 +91,15 @@ app.use(function(err, req, res, next) {
 if(jackd == null){
 	exec('sudo jackd -P75 -dalsa -dhw:1 -p1024 -n3 -s -r44100', function (error, stdout, stderr) {
 		console.log(stdout);
-
-		//start serial2osc
-		if(serial2osc == null){
-			exec('sudo /home/pi/prynth/serial2osc/serial2osc -s', function (error, stdout, stderr) {
-				console.log(stdout);
-			});
-		};
-
 	});
 };
 
-
-
+//start serial2osc
+if(serial2osc == null){
+	exec('sudo /home/pi/prynth/serial2osc/serial2osc -s', function (error, stdout, stderr) {
+		console.log(stdout);
+	});
+};
 
 //start sclang
 if(sclang == null) {
