1cad6be2b9c928aa1210c5d467891800b40d5772
Ivan Franco <ivan.almeida.franco@gmail.com>
2018-03-16 16:08:09 -0400

    added favicon

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.set('view engine', 'ejs');
 app.set('socketio', io);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
