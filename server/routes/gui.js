a02665b091cc581944157418ca51c7caef42f27f
Ivan Franco <if@Ivans-MacBook-Pro.local>
2018-10-19 13:47:14 -0400

    code improvements

--- server/routes/gui.js
+++ server/routes/gui.js
@@ -1,44 +1,40 @@
 var express = require('express');
 var router = express.Router();
-let multer = require('multer');
-let fs = require('fs');
-let path = require('path');
+var multer = require('multer');
+var fs = require('fs');
+var path = require('path');
 
-let storage = multer.diskStorage({
+var storage = multer.diskStorage({
 	destination: function (req, file, callback) {callback(null, (public_path + 'guifiles'));},
 	filename: function (req, file, callback) {callback(null, file.originalname);}
 });
 
-let upload = multer({ storage : storage}).array('filename', 10);
-
 var guifiles = [''];
 var private_path = path.join(__dirname, '../private/');
-let public_path = path.join(__dirname, '../public/');
+var public_path = path.join(__dirname, '../public/');
 
 
-router.get('/', function(req,res,next){
-    var configFile = fs.readFileSync(private_path +'config.json');
-    var configData = JSON.parse(configFile);
-    var hostname = configData.hostname;
+router.get('/', function(req,res){
+   let configFile = fs.readFileSync(private_path +'config.json');
+   let configData = JSON.parse(configFile);
+   let hostname = configData.hostname;
 
-	guifiles = fs.readdirSync(public_path + 'guifiles');
-	guifiles = guifiles.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+   guifiles = fs.readdirSync(public_path + 'guifiles');
+   guifiles = guifiles.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
 
-	res.render('gui', {
-		guifiles: guifiles,
-		hostname: hostname,
-	});
+   res.render('gui', {
+       guifiles: guifiles,
+       hostname: hostname,
+   });
 });
-
 
 router.post('/guifiles', function (req, res) {
 	if(req.body.action === 'load'){
-		let filetoload = guifiles[JSON.parse(req.body.fileindex)[0]];
-		let currentGUIString = fs.readFileSync((public_path + 'guifiles/' + filetoload), 'utf8');
-		let currentGUI = JSON.parse(currentGUIString);
-		console.log(currentGUIString);
-		res.io.emit('guidraw', currentGUI);
-	};
+		let filename = guifiles[JSON.parse(req.body.fileindex)[0]];
+		let guiBuffer = fs.readFileSync((public_path + 'guifiles/' + filename), 'utf8');
+		res.io.emit('guidraw', JSON.parse(guiBuffer));
+        res.sendStatus(200);
+    };
 
     if(req.body.action === 'save'){
         let filename = req.body.filename;
@@ -48,9 +44,9 @@ router.post('/guifiles', function (req, res) {
                 res.send('error saving file');
                 return console.log(err);
             } else {
-                var defaultGUIFile = req.body.filename;
-                var configFile = fs.readFileSync(private_path +'config.json');
-                var configData = JSON.parse(configFile);
+                let defaultGUIFile = req.body.filename;
+                let configFile = fs.readFileSync(private_path +'config.json');
+                let configData = JSON.parse(configFile);
 
                 configData.defaultGUIFile = defaultGUIFile;
                 var configJSON = JSON.stringify(configData, null, 2);
@@ -75,10 +71,9 @@ router.post('/guifiles', function (req, res) {
         let configFile = fs.readFileSync(private_path +'config.json');
         let configData = JSON.parse(configFile);
         let defaultGUIFile = configData.defaultGUIFile;
-        let currentGUIString = fs.readFileSync((public_path + 'guifiles/' + defaultGUIFile), 'utf8');
-        let currentGUI = JSON.parse(currentGUIString);
-        // console.log(currentGUIString);
-        res.io.emit('guidraw', currentGUI);
+        let guiBuffer = fs.readFileSync((public_path + 'guifiles/' + defaultGUIFile), 'utf8');
+        res.io.emit('guidraw', JSON.parse(guiBuffer));
+        res.sendStatus(200);
     };
 });
 
@@ -92,6 +87,5 @@ function refreshFiles() {
 	guifiles = fs.readdirSync(public_path + 'guifiles');
 	guifiles = guifiles.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
 };
-
 
 module.exports = router;
